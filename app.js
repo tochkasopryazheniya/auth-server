11c12dfbfe65f4d3aa078d8372d09e6bcde1c210
tochkasopryazheniya <tochkasopryazheniya@gmail.com>
2023-02-07 22:04:27 +0500

    9

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const errorMiddleWare = require('./middleware/error-middleware')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const PouchDB = require("pouchdb");
 
 const app = express();
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL
 }));
+
+const users = new PouchDB('datalake/users');
+const tokens = new PouchDB('datalake/tokens');
 
 app.use(logger('dev'));
 app.use(express.json());
