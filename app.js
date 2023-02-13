06a05dcf9bf160610a90d465c40d698e3885ffe4
tochkasopryazheniya <tochkasopryazheniya@gmail.com>
2023-02-13 11:29:54 +0500

    commit for jino server

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "https://auth-client-qa373h9zx-tochkasopryazheniya-gmailcom.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 })
 
