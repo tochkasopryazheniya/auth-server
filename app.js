6f6e13392d783995d67f3808ec2a27e3881b9b89
tochkasopryazheniya <tochkasopryazheniya@gmail.com>
2023-02-13 10:48:31 +0500

    commit for jino server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: '*',
+    origin: 'http://localhost:3000/',
     optionSuccessStatus:200
 }));
 
