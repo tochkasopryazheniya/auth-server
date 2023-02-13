ec57738ecb6b0a6cc53c9bb195d02760959a7343
tochkasopryazheniya <tochkasopryazheniya@gmail.com>
2023-02-13 11:17:00 +0500

    commit for jino server

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ const usersRouter = require('./routes/users');
 
 
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: 'https://auth-client-qa373h9zx-tochkasopryazheniya-gmailcom.vercel.app',
-    optionSuccessStatus:200
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: 'https://auth-client-qa373h9zx-tochkasopryazheniya-gmailcom.vercel.app',
+//     optionSuccessStatus:200
+// }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://auth-client-qa373h9zx-tochkasopryazheniya-gmailcom.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 
 
 app.use(logger('dev'));
